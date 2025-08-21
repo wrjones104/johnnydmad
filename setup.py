043aa261917222d1117d2b62802b52cc857a364c
Will Jones <wrjones104@users.noreply.github.com>
2025-08-21 06:54:10 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='johnnydmad',
     version='0.1.0',
-    packages=find_packages(),
+    packages=find_packages(where='.'),
+    install_requires=[
+        'numpy',
+        'mfvitools',
+        'PyYAML'
+    ],
     package_data={'': ['*.txt', '*.dat']},
     include_package_data=True,
 )
