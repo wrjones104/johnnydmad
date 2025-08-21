eacd647dfc963c3b980c598e54e58a89f1f2ae64
Will Jones <wrjones104@users.noreply.github.com>
2025-08-21 08:16:03 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='johnnydmad',
     version='0.1.0',
+    package_dir={'johnnydmad': ''},
     packages=find_packages(where='.'),
     install_requires=[
         'numpy',
